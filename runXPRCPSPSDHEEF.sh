23720a2720be6afb94b19707ce48c922f70aea9b
kameugne <rkameugne@gmail.com>
2024-03-01 11:29:54 +0100

    updated1

--- runXPRCPSPSDHEEF.sh
+++ runXPRCPSPSDHEEF.sh
@@ -1,7 +1,7 @@
 
 declare -a PROPConfig=("0" "1" "2" "3" "4" "5" "6" "7" "8" "9")
 declare -a SEARCHConfig=("0" "1" "2" "3")
-timeout=900
+timeout=600
 problem="TTHE_EF_rcpsp"  # TODO change the name of the problem
 # TODO put the path / command to launch the executable
 javaPath="/Library/Java/JavaVirtualMachines/jdk-17.0.2.jdk/Contents/Home/bin/java"
